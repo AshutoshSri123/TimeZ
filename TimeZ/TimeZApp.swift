35459c969afdda9eca219443bfebcb5a517ebd70
AshutoshSri123 <ashu.61003@gmail.com>
2025-08-26 23:43:06 +0530

    Initial Commit

---
+++ TimeZ/TimeZApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZApp.swift
+//  TimeZ
+//
+//  Created by Ashutosh Srivastava on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
